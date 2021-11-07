7e4b8ff76cb8dc7a31c211a1c3a6ea347ebf6352
Drew Nutter <dnut@users.noreply.github.com>
2021-11-07 14:23:03 +0100

    fix anchor client constructor name

--- client/src/load.rs
+++ client/src/load.rs
@@ -9,7 +9,7 @@ use solana_remote_wallet::remote_wallet::maybe_wallet_manager;
 
 pub fn program<'a>(payer: Rc<dyn Signer>) -> Program {
     let cluster = Cluster::Localnet;
-    let connection = anchor_client::Client::new_generic(cluster.clone(), payer.into());
+    let connection = anchor_client::Client::new(cluster.clone(), payer.into());
     connection.program(jet_governance::id())
 }
 
