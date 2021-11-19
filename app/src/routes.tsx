80b8f9ed2726afaf7ce800660599c67eb44b920e
Rheum rhabarbarum <32130807+rhubarbgarden@users.noreply.github.com>
2021-11-19 11:23:05 +0000

    JET-10 Proposals page styling changes, geoban

--- app/src/routes.tsx
+++ app/src/routes.tsx
@@ -1,5 +1,5 @@
 import { HashRouter, Route, Switch } from "react-router-dom";
-import React, { useMemo } from "react";
+import React, { useMemo, useState, useEffect } from "react";
 import { WalletProvider } from "@solana/wallet-adapter-react";
 import { ConnectionProvider } from "./contexts/connection";
 import { AccountsProvider } from "./contexts/accounts";
@@ -20,6 +20,8 @@ import {
 } from "@solana/wallet-adapter-wallets";
 
 export function Routes() {
+  const [geobanned, setGeobanned] = useState(false);
+
   const wallets = useMemo(
     () => [
       getPhantomWallet(),
@@ -39,6 +41,28 @@ export function Routes() {
     []
   );
 
+  // If IP address is in US, geoban
+  // useEffect(() => {
+  //   const getGeobanned = async () => {
+  //     const resp = await fetch("https://ipinfo.io/json?token=46ceefa5641a93", {
+  //       method: "GET",
+  //       headers: { "Content-Type": "application/json" },
+  //     });
+  //     let locale = await resp.json();
+  //     if (locale?.country === "US") {
+  //       setGeobanned(true);
+  //     }
+  //   };
+  //   getGeobanned();
+  // });
+  
+ const allProposals = INITIAL_STATE;
+const activeProposals = INITIAL_STATE.filter((p) => p.active);
+ const inactiveProposals = INITIAL_STATE.filter((p) => !p.active && p.result === "inactive");
+const passedProposals = INITIAL_STATE.filter((p) => !p.active && p.result === "passed");
+ const rejectedProposals = INITIAL_STATE.filter((p) => !p.active && p.result === "rejected");
+
+  
   return (
     <HashRouter basename={"/"}>
       <ConnectionProvider>
@@ -48,21 +72,33 @@ export function Routes() {
               <AppLayout>
                 <Switch>
                   <Route exact path="/">
-                    <HomeView proposals={INITIAL_STATE} />
+                    <HomeView
+                      allProposals={allProposals}
+                      activeProposals={activeProposals}
+                      inactiveProposals={inactiveProposals}
+                      passedProposals={passedProposals}
+                      rejectedProposals={rejectedProposals} />
                   </Route>
-                  {/* <Route exact path="/faucet">
-                    <FaucetView />
-                  </Route> */}
-                  {INITIAL_STATE.map((proposal) => <Route exact path={`/proposal/${proposal.id}/${proposal.headline.substring(0,7)}`}>
-                    <ProposalView
-                      id={proposal.id}
-                      result={proposal.result}
-                      headline={proposal.headline}
-                      active={proposal.active}
-                      end={proposal.end}
-                      hash={proposal.hash}
-                    />
-                  </Route>)}
+                  {geobanned
+                    ? null
+                    : INITIAL_STATE.map((proposal) => (
+                        <Route
+                          exact
+                          path={`/proposal/${
+                            proposal.id
+                          }/${proposal.headline.substring(0, 7)}`}
+                        >
+                          <ProposalView
+                            id={proposal.id}
+                            result={proposal.result}
+                            headline={proposal.headline}
+                            active={proposal.active}
+                            end={proposal.end}
+                          hash={proposal.hash}
+                          shownProposals={activeProposals}
+                          />
+                        </Route>
+                      ))}
                 </Switch>
               </AppLayout>
             </MarketProvider>
