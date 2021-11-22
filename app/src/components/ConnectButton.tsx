6ba6d00a717d7720468fe2058e4a629bc91c3870
Rheum rhabarbarum <32130807+rhubarbgarden@users.noreply.github.com>
2021-11-22 22:01:48 +0000

    JET-10 Tweaked connect button

--- app/src/components/ConnectButton.tsx
+++ app/src/components/ConnectButton.tsx
@@ -1,63 +1,41 @@
-import { useWalletModal } from "@solana/wallet-adapter-ant-design";
+import React, { FunctionComponent, useMemo } from "react";
 import { useWallet } from "@solana/wallet-adapter-react";
-import { Button, Dropdown, Menu } from "antd";
-import { ButtonProps } from "antd/lib/button";
-import React, { useCallback } from "react";
-import { LABELS } from "../constants";
+import { shortenAddress } from "../utils/utils";
+import { useConnectWallet } from "../contexts/connectWallet";
+import { Button } from "antd";
 
-export interface ConnectButtonProps
-  extends ButtonProps,
-    React.RefAttributes<HTMLElement> {
-  allowWalletChange?: boolean;
-}
+export const ConnectButton: FunctionComponent = () => {
+  const { connected, wallet, publicKey, disconnect } = useWallet();
+  const { setConnecting } = useConnectWallet();
 
-export const ConnectButton = (props: ConnectButtonProps) => {
-  const { setVisible } = useWalletModal();
-
-  const { connected, connect, select, wallet, wallets } = useWallet();
-  const { onClick, children, disabled, allowWalletChange, ...rest } = props;
-
-  const handleChangeWalletButtonClick: React.MouseEventHandler<HTMLElement> = useCallback(
-    (event) => {
-      if (connected) {
-        onClick?.(event);
-        return;
-      }
-      setVisible(true);
-    },
-    [setVisible, onClick, connected]
+  // Dynamic connection properties for the button
+  const connectButtonTitle = useMemo(() => 
+    connected ? "Disconnect" : "Connect",
+    [connected]
   );
-
-  // only show if wallet selected or user connected
-  const menu = (
-    <Menu>
-      {wallets.map((wallet) => (
-        <Menu.Item key={wallet.name} onClick={() => select(wallet.name)}>
-          Change Wallet to {wallet.name}
-        </Menu.Item>
-      ))}
-    </Menu>
+  const connectButtonClasses = useMemo(() =>
+    `connect-btn flex-centered ${connected ? '' : 'small-btn bicyclette'}`,
+    [connected]
   );
-
-  if (!wallet || !allowWalletChange) {
-    return (
-      <Button
-        {...rest}
-        onClick={handleChangeWalletButtonClick}
-        disabled={connected && disabled}
-      >
-        {connected ? props.children : LABELS.CONNECT_LABEL}
-      </Button>
-    );
-  }
+  const connectButtonText = useMemo(() => 
+    publicKey 
+      ? `${shortenAddress(publicKey.toString())} Connected` 
+        : "Connect",
+    [publicKey]
+  );
 
   return (
-    <Dropdown.Button
-      onClick={connected ? onClick : connect}
-      disabled={connected && disabled}
-      overlay={menu}
-    >
-      {LABELS.CONNECT_LABEL}
-    </Dropdown.Button>
+    <Button className={connectButtonClasses}
+      title={connectButtonTitle}
+      onClick={() => connected ? disconnect() : setConnecting(true)}>
+      {connected && (
+        <img src={`img/wallets/${wallet?.name.toLowerCase()}.png`} 
+          width="20px"
+          height="auto"
+          alt={wallet?.name}
+        />
+      )}
+      {connectButtonText}
+    </Button>
   );
 };
