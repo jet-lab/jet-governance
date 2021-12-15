dc5636c9bd1baf2cf52c59ea61a447bd2103f500
Rheum rhabarbarum <32130807+rhubarbgarden@users.noreply.github.com>
2021-12-15 21:30:52 +0000

    JET-303 Added styling to proposal page

--- app/src/components/proposal/Stakeholders.tsx
+++ app/src/components/proposal/Stakeholders.tsx
@@ -9,21 +9,13 @@ export const Stakeholders = (props: {
   thisUser?: boolean
 }) => {
   const { type, amount, address, thisUser } = props;
-  const [color, setColor] = useState("hsla(0, 1%, 62%, 1)")
   const [vote, setVote] = useState("abstain")
-  const [gradient, setGradient] = useState(false);
-  const [symbol, setSymbol] = useState("");
 
   useEffect(() => {
     if (type === "inFavor") {
     setVote("in favor")
-    setColor("var(--gradient)")
-    setGradient(true);
-    setSymbol("fas fa-thumbs-up")
   } else if (type === "against") {
     setVote("against")
-    setColor("var(--failure)")
-    setSymbol("fas fa-thumbs-down")
   }
   }, [type])
 
@@ -31,7 +23,7 @@ export const Stakeholders = (props: {
     <div className={`stakeholders ${thisUser && 'your-vote'}`} >
       <span className="address">{shortenAddress(address)}</span>
       <span className="amount">{abbreviateNumber(amount, 2)} JET</span>
-      <span style={{color: color}} className={`vote ${gradient ? "text-gradient" : ""}`}>{vote} <i className={symbol}></i></span>
+      <span className={`vote`}>{vote}</span>
     </div>
   );
 };
