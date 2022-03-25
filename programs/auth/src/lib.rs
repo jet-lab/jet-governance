6cbc7fa45f1b652fc9f9787241238e9d0c1f338d
eyevzz <norman@jetprotocol.io>
2022-03-25 14:09:44 +0000

    Adds events for user auth

--- programs/auth/src/lib.rs
+++ programs/auth/src/lib.rs
@@ -1,5 +1,7 @@
 use anchor_lang::prelude::*;
 
+mod events;
+
 declare_id!("JPALXR88jy2fG3miuu4n3o8Jef4K2Cgc3Uypr3Y8RNX");
 
 /// Hardcoded address of the authority that can authenticate users
@@ -75,6 +77,10 @@ pub mod jet_auth {
         auth.complete = false;
         auth.allowed = false;
 
+        emit!(events::CreateAuthAccount {
+            user: auth.owner
+        });
+
         Ok(())
     }
 
@@ -84,6 +90,10 @@ pub mod jet_auth {
 
         auth.complete = true;
         auth.allowed = true;
+
+        emit!(events::Authenticate {
+            user: auth.owner
+        });
 
         Ok(())
     }
