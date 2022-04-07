13280c8e3b66b6a65c974a3d97f7ed45d2fcc604
Tristyn Stimpson <tristynstimpson@gmail.com>
2022-04-07 05:43:33 -0700

    Fix init stake account (#60)
    * Fix init stake account
    
    * Update realm and governance for new stakepool
    
    Co-authored-by: Rheum rhabarbarum <32130807+rhubarbgarden@users.noreply.github.com>

--- app/src/utils/ids.ts
+++ app/src/utils/ids.ts
@@ -4,8 +4,8 @@ export let JET_TOKEN_MINT = new PublicKey("JET6zMJWkCN9tpRT2v2jfAmm5VnQFDpUBCyaK
 export const JET_FAUCET_DEVNET = new PublicKey("4RqY4p1xXMcMF1xrGYtJG8aaQDR7s8GHkjfSHFqcBmQV");
 export const COUNCIL_TOKEN_MINT = new PublicKey("GBZ2J49hpCLM8HDFav7gQ2zJNC3LCV264y42WhoqdEYo");
 export const COUNCIL_FAUCET_DEVNET = new PublicKey("3AJLr1kbsC8btrizbYTWPCvfTfCJpK7JYHHpmZdy7sgM");
-export let JET_REALM = new PublicKey("DpfKN6p2LgeL79hrJTjHBpJJsKwjziswjjyPhHGqiock");
-export let JET_GOVERNANCE = new PublicKey("F944EEYVbztkh3aKNDnjpeSoky77nEMfZ58MN3JoPnsC");
+export let JET_REALM = new PublicKey("34EMxuDbQuVGsBqjabYZqgC24VuBr12oXRt3sRgFvFdT");
+export let JET_GOVERNANCE = new PublicKey("2frnZUjqy1FkVCV8Ypd3FUFX6NcNEdbxHwNAJfYZsEAz");
 export const GOVERNANCE_PROGRAM_ID = new PublicKey("JPGovTiAUgyqirerBbXXmfyt3SkHVEcpSAPjRCCSHVx");
 export let BPF_UPGRADE_LOADER_ID = new PublicKey("BPFLoaderUpgradeab1e11111111111111111111111");
 
@@ -15,8 +15,8 @@ export const setProgramIds = (envName: string) => {
     JET_GOVERNANCE = PublicKey.default; // FIXME!
     JET_TOKEN_MINT = new PublicKey("JET6zMJWkCN9tpRT2v2jfAmm5VnQFDpUBCyaKojmGtz");
   } else if (envName === "devnet") {
-    JET_REALM = new PublicKey("DpfKN6p2LgeL79hrJTjHBpJJsKwjziswjjyPhHGqiock");
-    JET_GOVERNANCE = new PublicKey("F944EEYVbztkh3aKNDnjpeSoky77nEMfZ58MN3JoPnsC");
+    JET_REALM = new PublicKey("34EMxuDbQuVGsBqjabYZqgC24VuBr12oXRt3sRgFvFdT");
+    JET_GOVERNANCE = new PublicKey("2frnZUjqy1FkVCV8Ypd3FUFX6NcNEdbxHwNAJfYZsEAz");
     JET_TOKEN_MINT = new PublicKey("FRuFWBrp1Kh6LpAi9CRvjk97C6YpCR7AERq62N2CZFUg");
   }
 };
