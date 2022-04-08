a33aaad124c26e94852c97ee1cdc2849e2f9d6c0
Qiqi Zheng <68657634+qiqicodes@users.noreply.github.com>
2022-04-08 15:16:43 -0700

    [GVN-360]Expired unbonding log (#48)
    * update action log and button style
    
    * fix countdown text
    
    * update unbonding log when timer expires

--- app/src/components/unbonding/UnbondingLog.tsx
+++ app/src/components/unbonding/UnbondingLog.tsx
@@ -16,15 +16,17 @@ export const UnbondingLog = ({ unbondingAccount }: { unbondingAccount: Unbonding
   const [isUnbonded, setIsUnbonded] = useState(false);
   const { getAccountExplorerUrl } = useBlockExplorer();
 
-  useEffect(() => {
-    const unbondedState = UnbondingAccount.isUnbonded(unbondingAccount);
-    return setIsUnbonded(unbondedState);
-  }, [setIsUnbonded, unbondingAccount]);
-
   const currentTime = useCurrentTime();
   const oneDayCountdown =
     !isUnbonded &&
     bnToNumber(unbondingAccount.unbondingAccount.unbondedAt) * 1000 - currentTime < ONE_DAY;
+
+  const expired = bnToNumber(unbondingAccount.unbondingAccount.unbondedAt) * 1000 <= currentTime;
+
+  useEffect(() => {
+    const unbondedState = UnbondingAccount.isUnbonded(unbondingAccount);
+    return setIsUnbonded(unbondedState);
+  }, [setIsUnbonded, unbondingAccount, currentTime]);
 
   const getUnbondingAccountExplorerUrl = () => {
     unbondingAccount &&
@@ -57,14 +59,15 @@ export const UnbondingLog = ({ unbondingAccount }: { unbondingAccount: Unbonding
 
       <td className="action italics-text">
         <i onClick={getUnbondingAccountExplorerUrl}>
-          {oneDayCountdown
-            ? `Unstake complete in ${getRemainingTime(
+          Unstake complete{" "}
+          {oneDayCountdown && !expired
+            ? `in ${getRemainingTime(
                 currentTime,
                 bnToNumber(unbondingAccount.unbondingAccount.unbondedAt) * 1000
               )}`
-            : `Unstake complete on
+            : `on 
             ${dateFromUnixTimestamp(unbondingAccount.unbondingAccount.unbondedAt)}`}
-        </i>{" "}
+        </i>
         <Button
           size="small"
           type={isUnbonded ? undefined : "dashed"}
