942ceeb21c0a7affc2473a1d9582db194de7f70c
Rheum rhabarbarum <32130807+rhubarbgarden@users.noreply.github.com>
2022-04-08 23:49:11 +0800

    Update unstake modal text

--- app/src/components/modals/UnstakeModal.tsx
+++ app/src/components/modals/UnstakeModal.tsx
@@ -109,17 +109,6 @@ export const UnstakeModal = ({
     closable: true,
     content: (
       <div className="flex column">
-        {unrelinquishedVoteRecords && unrelinquishedVoteRecords.length !== 0 && (
-          <p>
-            You currently have votes cast on active proposals. When you unstake ANY amount of JET
-            tokens that have already voted, ALL of your active votes are rescinded. Even if you only
-            unstaked a portion of your staked JET,{" "}
-            <b>
-              please immediately revote on any active proposals with any remaining staked JET you
-              still have after this unstaking.
-            </b>
-          </p>
-        )}
         <p>
           Unstaked tokens have a 29.5-day unbonding period. During this period, you will not earn
           any rewards.
