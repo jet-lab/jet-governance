877967b7f1422b5886d11de63a224d3d8cde1790
Matthew Callens <callensmatt@gmail.com>
2022-04-19 10:19:43 -0400

    add `serde::Serialize` impls for account structs (#102)
    * add serde serialization implementations for new `cli` feature on program account structs
    
    * fix dep: feature prefix
    
    * update serializations to be strings for pubkeys
    
    * remove null bytes from descriptions on airdrop serializations

--- programs/staking/src/state.rs
+++ programs/staking/src/state.rs
@@ -1,6 +1,7 @@
-use std::convert::TryInto;
-
 use anchor_lang::prelude::*;
+#[cfg(feature = "cli")]
+use serde::ser::{Serialize, SerializeStruct, Serializer};
+use std::convert::TryInto;
 
 use crate::spl_addin::{MaxVoterWeightRecord, VoterWeightAction, VoterWeightRecord};
 use crate::ErrorCode;
@@ -165,10 +166,42 @@ impl StakePool {
     }
 }
 
+#[cfg(feature = "cli")]
+impl Serialize for StakePool {
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut s = serializer.serialize_struct("StakePool", 10)?;
+        s.serialize_field("authority", &self.authority.to_string())?;
+        s.serialize_field("tokenMint", &self.token_mint.to_string())?;
+        s.serialize_field("stakePoolVault", &self.stake_pool_vault.to_string())?;
+        s.serialize_field(
+            "maxVoterWeightRecord",
+            &self.max_voter_weight_record.to_string(),
+        )?;
+        s.serialize_field("governanceRealm", &self.governance_realm.to_string())?;
+        s.serialize_field(
+            "stakeCollateralMint",
+            &self.stake_collateral_mint.to_string(),
+        )?;
+        s.serialize_field("unbondPeriod", &self.unbond_period)?;
+        s.serialize_field("vaultAmount", &self.vault_amount)?;
+        s.serialize_field("bonded", &self.bonded)?;
+        s.serialize_field("unbonding", &self.unbonding)?;
+        s.end()
+    }
+}
+
 /// Primitive that represents a pool of tokens with ownership of a portion
 /// of the pool represented by shares. Each SharedTokenPool has a distinct
 /// value for its own shares.
 #[derive(Default, Debug, Copy, Clone, AnchorSerialize, AnchorDeserialize)]
+#[cfg_attr(
+    feature = "cli",
+    derive(serde::Serialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct SharedTokenPool {
     /// Number of tokens held by this pool
     tokens: u64,
@@ -418,6 +451,22 @@ impl StakeAccount {
     }
 }
 
+#[cfg(feature = "cli")]
+impl Serialize for StakeAccount {
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut s = serializer.serialize_struct("StakeAccount", 5)?;
+        s.serialize_field("owner", &self.owner.to_string())?;
+        s.serialize_field("stakePool", &self.stake_pool.to_string())?;
+        s.serialize_field("voterWeightRecord", &self.voter_weight_record.to_string())?;
+        s.serialize_field("bondedShares", &self.bonded_shares)?;
+        s.serialize_field("unbondingShares", &self.unbonding_shares)?;
+        s.end()
+    }
+}
+
 #[account]
 #[derive(Default)]
 pub struct UnbondingAccount {
@@ -430,6 +479,20 @@ pub struct UnbondingAccount {
 
     /// The time after which the staked amount can be withdrawn
     pub unbonded_at: i64,
+}
+
+#[cfg(feature = "cli")]
+impl Serialize for UnbondingAccount {
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut s = serializer.serialize_struct("UnbondingAccount", 3)?;
+        s.serialize_field("stakeAccount", &self.stake_account.to_string())?;
+        s.serialize_field("shares", &self.shares)?;
+        s.serialize_field("unbondedAt", &self.unbonded_at)?;
+        s.end()
+    }
 }
 
 #[cfg(test)]
