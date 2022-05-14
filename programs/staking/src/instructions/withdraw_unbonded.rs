957bad28319ffb71c5b6efa129bde35bd0f2b4a3
eyevzz <norman@jetprotocol.io>
2022-05-14 13:35:06 +0100

    Gardening

--- programs/staking/src/instructions/withdraw_unbonded.rs
+++ programs/staking/src/instructions/withdraw_unbonded.rs
@@ -74,7 +74,7 @@ pub fn withdraw_unbonded_handler(ctx: Context<WithdrawUnbonded>) -> Result<()> {
     let withdrawn_amount = stake_pool.withdraw_unbonded(stake_account, unbonding_account);
 
     unbonding_account.stake_account = Pubkey::default();
-    
+
     let stake_pool = &ctx.accounts.stake_pool;
     let unbonding_account = &ctx.accounts.unbonding_account;
 
