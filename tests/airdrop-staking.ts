ed0a5464269602a452137b877019dfc6119493ff
Rheum rhabarbarum <32130807+rhubarbgarden@users.noreply.github.com>
2022-06-20 21:59:26 +0800

    Prettier

--- tests/airdrop-staking.ts
+++ tests/airdrop-staking.ts
@@ -1,11 +1,4 @@
-import {
-  Program,
-  AnchorError,
-  Provider,
-  setProvider,
-  workspace,
-  BN
-} from "@project-serum/anchor";
+import { Program, AnchorError, Provider, setProvider, workspace, BN } from "@project-serum/anchor";
 import NodeWallet from "@project-serum/anchor/dist/cjs/nodewallet";
 import {
   Keypair,
